c0d876f5fc6c324ed4fdcaa4aef1a5683f4b4edb
Gabriel Beltran <gabrielaudbeltran23@gmail.com>
2022-09-05 19:50:53 -0600

    First Initial Commit

---
+++ EncryptionTest.cs
@@ -0,0 +1,17 @@
+namespace PracticeClass;
+
+using System;
+using PracticeClass;
+
+public class EncryptionTest
+{
+    public static void Main(String[] args)
+    {
+        
+    }
+
+    public static void InitiateFirstEncryption()
+    {
+        
+    }
+}
